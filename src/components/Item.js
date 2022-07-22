cb2737678768fcad408a1f83b1e4365a8cce64b4
Dawn Olivier <dmarieolivier@gmail.com>
2022-07-22 01:05:45 -0400

    commit tree 8142f39b39dd00eb0b6bbfd153e4e403d90cdef2

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [item, setItem] = useState("")
+
+  function handleItem() {
+    setItem((item) => !item);
+  }
+
+  const cart = item ? "in-cart" : "";
+  const btnBg = item ? "remove" : "add";
+  
   return (
-    <li className="">
+    <li className={cart}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={btnBg} onClick = {handleItem}>
+        {item ? "Remove From Cart" : "Add Cart"}
+      </button>
     </li>
   );
 }
